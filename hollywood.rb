562e168cd7fcec1a1c12b1b48e69a4461d9ebc4d
Thomas Ritter <thomas.ritter@mocoapp.com>
2019-07-15 18:59:50 +0200

    Initial code state

---
+++ hollywood.rb
@@ -0,0 +1,49 @@
+# ================================
+#     Welcome to Hollywood !
+# ================================
+#
+# This is how this excercise works:
+#
+# There are two files:
+#
+# * `hollywood.rb` (this one)
+# * `hollywood_test.rb` (the other one)
+#
+# Both files should be in the same directory!
+# Now, in your terminal go into that directory and run the test file, like so:
+#
+# `ruby hollywood_test.rb`
+#
+# This should output many things, but most importantly the last
+# line should read something like this:
+#
+# `3 runs, 0 assertions, 0 failures, 3 errors, 0 skips`
+#
+# ...and this is where our work starts. After succesfully completing
+# this excercise the command above shold return
+#
+# `3 runs, 3 assertions, 0 failures, 0 errors, 0 skips`
+#
+# And here's what you need to do.
+# Below there are **three empty classes**
+#
+# * `Actor`
+# * `Character`
+# * `Movie`
+#
+# Have a look at the `def setup` part in `hollywood_test.rb` in order to get an idea how
+# these classes are supposed to be "connected" to each other.
+#
+# You need to put code into theses classes so that test file passes.
+# In order figure out "what" you need to code, try to follow the error messages from the test runs.
+# In this way you'll solve the excercise little by little.
+#
+
+class Actor
+end
+
+class Character
+end
+
+class Movie
+end
---
+++ hollywood_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require_relative 'hollywood'
+
+# In order to solve the "Hollywood" excercise you don't have to
+# edit this file.
+
+class HollywoodTest < Minitest::Test
+  def setup
+    @groundhog_day = Movie.new('Groundhog Day', 1993)
+
+    @bill = Actor.new('Bill Murray')
+    @andie = Actor.new('Andie MacDowell')
+
+    @bills_character = Character.new(@bill, 'Phil Connors', 'male')
+    @andies_character = Character.new(@andie, 'Rita Hanson', 'female')
+
+    @groundhog_day.male_appearance = @bills_character
+    @groundhog_day.female_appearance = @andies_character
+  end
+
+  def test_actors_names
+    assert_equal 'Bill Murray', @bill.name
+    assert_equal 'Andie MacDowell', @andie.name
+  end
+
+  def test_bills_character
+    assert_equal 'Phil Connors', @bills_character.name
+    assert_equal 'male', @bills_character.sex
+    assert_equal 'Bill Murray', @bills_character.actor.name
+    # we dont test Andie's character (but we could!)
+  end
+
+  def test_groundhog_day
+    assert_equal 'Groundhog Day', @groundhog_day.name
+    assert_equal 1993, @groundhog_day.release_year
+    assert_equal 'Rita Hanson', @groundhog_day.female_appearance.name
+    assert_equal 'Andie MacDowell', @groundhog_day.female_appearance.actor.name
+    # we dont test Bill's character (but we could!)
+  end
+end
